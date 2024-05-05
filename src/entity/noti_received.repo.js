c4e247e8d62722797138b8262c3b926bec944d14
twodollars123 <twodollars.no2002@gmail.com>
2024-05-05 18:21:31 +0700

    tuanv-hotfix

--- src/entity/noti_received.repo.js
+++ src/entity/noti_received.repo.js
@@ -13,7 +13,7 @@ class NotiReceivedRepo {
 
   getAllByUserId = async (received_id) => {
     const query = {
-      text: "select * from notifications n inner join noti_types nt on n.noti_typeid = nt.noti_typeid inner join noti_received nr on n.noti_id = nr.noti_id where nr.received_id in (-1, $1) and nr.status = 0 order by nr.createat asc",
+      text: "select * from notifications n inner join noti_types nt on n.noti_typeid = nt.noti_typeid inner join noti_received nr on n.noti_id = nr.noti_id inner join users u on n.noti_senderid = u.user_id  where nr.received_id in (-1, $1) and nr.status = 0 order by nr.createat asc",
       values: [received_id],
     };
     const listNoti = (await client.query(query)).rows;
